27c362d6db2b18fc448eef463354e8132a3cb745
ozlem <06ozlemozturk@gmail.com>
2023-07-30 23:16:45 +0300

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,52 @@
+import React from "react";
+
+import {
+  HeaderButton,
+  HeaderContainer,
+  HeaderFrom,
+  HeaderTittle,
+  SearchInput,
+  SelectBox,
+} from ".Header.style";
+import { useContext } from "../../context/BooksContext";
+
 const Header = () => {
+  const printType = ["all", "books", "magazines"];
+  const { SearchInfo, setSearchInfo, getData } = useBooksContext();
+
+  const handleChange = (e) => {
+    console.log(e.target);
+    console.log(e.target.name);
+    console.log({ [e.target.name]: e.target.value });
+    setSearchInfo({ ...SearchInfo, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  };
+  console.log(SearchInfo);
   return (
     <HeaderContainer>
-      <HeaderTitle>BOOKS OR MAGAZINES</HeaderTitle>
-      <HeaderFrom>
-        <SearchInput type="search" placeholder="Search..." />
-        <selecBox>
-          <option>All</option>
-        </selecBox>
-        <HeaderButton>Search</HeaderButton>
+      <HeaderTittle>BOOKS OR MAGAZINES</HeaderTittle>
+      <HeaderFrom onSubmit={handleSubmit}>
+        <SearchInput
+          type="search"
+          placeholder="Search..."
+          name="query"
+          value={SearchInfo.query}
+          onChange={handleChange}
+          required
+        />
+        <SelectBox>
+          value={searchInfo.selectType}
+          name="selectType" onChange={handleChange}
+          {printType.map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+          <HeaderButton>Search</HeaderButton>
+        </SelectBox>
       </HeaderFrom>
     </HeaderContainer>
   );
--- src/components/navbar/Navbar.style.jsx
+++ src/components/navbar/Navbar.style.jsx
@@ -21,3 +21,4 @@ export const HamburgerIcon = styled.div`
     display: block;
   }
 `;
+export const MENU = styled(Flex)``;
