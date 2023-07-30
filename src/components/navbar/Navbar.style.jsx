aa5b16b15478ce2ec1d3823b6827d52d5ee10fb2
ozlem <06ozlemozturk@gmail.com>
2023-07-30 23:32:33 +0300

    navstyle

--- src/components/navbar/Navbar.style.jsx
+++ src/components/navbar/Navbar.style.jsx
@@ -1,7 +1,12 @@
+import styled from "styled-components";
+import Flex from "../../styles/Flex";
+import { NavLink } from "react-router-dom";
+
 export const Nav = styled(Flex)`
   background-color: ${({ theme }) => theme.colors.navbarBgColor};
   padding: 1rem 2rem;
 `;
+
 export const Logo = styled(NavLink)`
   padding: 1rem 0;
   text-decoration: none;
@@ -13,7 +18,6 @@ export const Logo = styled(NavLink)`
     color: ${({ theme }) => theme.colors.linkHoverColor};
   }
 `;
-
 export const HamburgerIcon = styled.div`
   display: none;
   cursor: pointer;
@@ -21,9 +25,27 @@ export const HamburgerIcon = styled.div`
     display: block;
   }
 `;
-export const MENU = styled(Flex)`
- @media screen {
-  
- }(max-width: ${({ theme }) => theme.screens.lg)}
+export const Menu = styled(Flex)`
+  @media (max-width: ${({ theme }) => theme.screens.lg}) {
+    flex-direction: column;
+    width: 100%;
+    transition: all 1s;
+    max-height: ${({ showMenu }) => (showMenu ? "330px" : "0px")};
+    overflow: hidden;
+  }
+`;
 
+export const MenuLink = styled(NavLink)`
+  text-align: center;
+  padding: 1rem 2rem;
+  cursor: pointer;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.linkColor};
+  transition: all 0.3s ease-in;
+  :hover {
+    color: ${({ theme }) => theme.colors.linkHoverColor};
+  }
+  &.active {
+    text-decoration: underline;
+  }
 `;
